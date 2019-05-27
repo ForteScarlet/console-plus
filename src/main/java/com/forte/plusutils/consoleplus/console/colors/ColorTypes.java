8fc3aedfb5a1c46924b548871c10c5ea2e9a4462
ForteScarlet <ForteScatlet@163.com>
2019-05-27 17:36:10 +0800

    update package

---
+++ src/main/java/com/forte/plusutils/consoleplus/console/colors/ColorTypes.java
@@ -0,0 +1,19 @@
+package com.forte.plusutils.consoleplus.console.colors;
+
+/**
+ * @author ForteScarlet <[163邮箱地址]ForteScarlet@163.com>
+ * @since JDK1.8
+ **/
+public interface ColorTypes {
+
+    int getColorIndex();
+
+    default boolean isBackGround(){
+        return this instanceof BackGroundColorTypes;
+    }
+
+    default boolean isFont(){
+        return this instanceof FontColorTypes;
+    }
+
+}
---
+++ src/main/java/com/forte/plusutils/consoleplus/system/ColorSystem.java
@@ -0,0 +1,16 @@
+package com.forte.plusutils.consoleplus.system;
+
+import java.io.PrintStream;
+
+/**
+ * 颜色控制台打印
+ * @author ForteScarlet <[163邮箱地址]ForteScarlet@163.com>
+ * @since JDK1.8
+ **/
+public class ColorSystem {
+
+    private static PrintStream out;
+    private static PrintStream err;
+
+
+}
