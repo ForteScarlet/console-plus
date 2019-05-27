ecedd730b6a07950e06b32b973072907ca9c2aa2
ForteScarlet <ForteScatlet@163.com>
2019-05-27 17:31:28 +0800

    :tada: Initial commit.

---
+++ src/main/java/com/forte/utils/consoleplus/ConsolePlus.java
@@ -0,0 +1,58 @@
+package com.forte.utils.consoleplus;
+
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
+import java.lang.reflect.Field;
+import java.util.function.Function;
+
+/**
+ * 控制台PLUS
+ * - 获取彩色输出器
+ * - 改变{@link System}的out、err
+ * @author ForteScarlet <[163邮箱地址]ForteScarlet@163.com>
+ * @since JDK1.8
+ **/
+public class ConsolePlus {
+
+    /**
+     * 取代System.out对象
+     * @throws UnsupportedEncodingException
+     * @throws IllegalAccessException
+     */
+    public static void updatePrint(Function<String, String> printFunction) throws UnsupportedEncodingException, IllegalAccessException {
+        PrintStream out = System.out;
+        FortePlusPrintStream fortePlusPrintStream = new FortePlusPrintStream(new OutputStream() {
+            @Override
+            public void write(int b){}
+        }, true, "UTF-8");
+
+
+        //PrintStream的参数
+        Field[] declaredFields = PrintStream.class.getDeclaredFields();
+        for (Field field : declaredFields) {
+            field.setAccessible(true);
+            Object o = field.get(out);
+            field.set(fortePlusPrintStream, o);
+        }
+
+        if(printFunction != null){
+            fortePlusPrintStream.setPrintFunction(printFunction);
+        }
+
+        //取代对象
+        System.setOut(fortePlusPrintStream);
+    }
+
+    /**
+     * 取代System.out对象
+     * @throws UnsupportedEncodingException
+     * @throws IllegalAccessException
+     */
+    public static void updatePrint() throws UnsupportedEncodingException, IllegalAccessException {
+        updatePrint(null);
+    }
+
+
+
+}
---
+++ src/main/java/com/forte/utils/consoleplus/console/Colors.java
@@ -0,0 +1,47 @@
+package com.forte.utils.consoleplus.console;
+
+import com.forte.utils.consoleplus.console.colors.BackGroundColorTypes;
+import com.forte.utils.consoleplus.console.colors.ColorTypes;
+import com.forte.utils.consoleplus.console.colors.FontColorTypes;
+
+/**
+ * 控制台可以用的颜色
+ * @author ForteScarlet <[163邮箱地址]ForteScarlet@163.com>
+ * @since JDK1.8
+ **/
+public class Colors {
+
+    /** 背景色 */
+    public final static BackGroundColorTypes.BackGroundColor BACK_GROUND = BackGroundColorTypes.BackGroundColor.getInstance();
+    /** 字体色 */
+    public final static FontColorTypes.FontColor FONT = FontColorTypes.FontColor.getInstance();
+
+    /** 颜色字体 */
+    private final String COLOR_STR;
+
+    /** 按照顺序的颜色数组 */
+    private final ColorTypes[] COLORS;
+
+    /**构造 */
+    Colors(String str, ColorTypes[] colors){
+        this.COLOR_STR = str;
+        this.COLORS = colors;
+    }
+
+    /** 获取一个色彩字构建器 */
+    public static ColorsBuilder builder(){
+        return ColorsBuilder.getInstance();
+    }
+
+    public ColorTypes[] getColorTypes(){
+        return COLORS;
+    }
+
+    /**
+     * 重写toString
+     */
+    @Override
+    public String toString(){
+        return COLOR_STR;
+    }
+}
